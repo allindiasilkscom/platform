72de260450b4a9ff3564274996847cdeb6e77680
allindiasilkscom <153381149+allindiasilkscom@users.noreply.github.com>
2024-05-08 01:07:21 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Header = () => {
+  return (
+    <header className='h-16 shadow-md'>
+      <div className='h-full container mx-auto flex items-center px-4'>
+    <div className=''>
+      <Logo w={90} h={50}/>
+    </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
